467a4b69940440966c9d4571cc64279b5e331762
pepposj <pepposj@miamioh.edu>
2024-12-04 19:24:48 -0500

    Added text_board view. Modified main.py to run either graphical or text based (hard coded for now). Modified game_controller.py to account for text based changed. Overhaul in handling board changes by using a refresh method that handles which view is being used. Timer only updates during each move. Restarting after winning/losing is not working.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Models.board_model import BoardModel
 from Controllers.game_controller import GameController
 from Views.board_view import BoardView
+from Views.text_board_view import TextBoardView
 
 if __name__=="__main__":
     rows = 10
@@ -8,7 +9,13 @@ if __name__=="__main__":
     mines = 10
 
     board = BoardModel(rows, cols, mines)
-    view = BoardView(board)
+    # view = BoardView(board)  # Graphical view
+    view = TextBoardView(board)  # Text-based view
     controller = GameController(board, view)
     view.controller = controller
-    view.window.mainloop()
+
+    if isinstance(view, TextBoardView):
+        while not controller.game_over:
+            view.promptMove()
+    else: 
+        view.window.mainloop()
